61113b79c22af6a6fb0150a36613e8dbb8cda4d4
karim-ayite <karim.ayite@gmail.com>
2022-05-10 15:25:06 +0000

    add start game button and his test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,51 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {HarnessLoader} from '@angular/cdk/testing';
+import {TestbedHarnessEnvironment} from '@angular/cdk/testing/testbed';
+import {AppModule} from "./app.module";
+import {MatButtonHarness} from '@angular/material/button/testing';
+
+let loader: HarnessLoader;
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+  // beforeEach(async () => {
+  //   await TestBed.configureTestingModule({
+  //     declarations: [
+  //       AppComponent
+  //     ],
+  //   }).compileComponents();
+  // });
+  //
+  // it('should create the app', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app).toBeTruthy();
+  // });
+  //
+  // it(`should have as title 'draughts-ui'`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app.title).toEqual('draughts-ui');
+  // });
+  //
+  // it('should render start game button', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   expect(compiled.querySelector('.content span')?.textContent).toContain('Start game');
+  // });
+  describe('my-component', () => {
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({imports: [AppModule], declarations: [AppComponent]})
+        .compileComponents();
+      let fixture = TestBed.createComponent(AppComponent);
+      loader = TestbedHarnessEnvironment.loader(fixture);
+    });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'draughts-ui'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('draughts-ui');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('draughts-ui app is running!');
-  });
+    it('should display a start game button', async () => {
+      const startGameButton = await loader.getHarness(MatButtonHarness.with({selector: '#start-game'}));
+      expect(startGameButton).toBeDefined();
+      expect(await startGameButton.getText()).toEqual("Start game")
+    });
+  })
 });
